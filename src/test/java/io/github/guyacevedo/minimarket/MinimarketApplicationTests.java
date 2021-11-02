5a078249bf44d75b88d26630bfd698e3e65769a1
guyacevedo <guy.acevedoa@gmail.com>
2021-11-01 20:47:52 -0500

    first commit, proyect created.

---
+++ src/test/java/io/github/guyacevedo/minimarket/MinimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guyacevedo.minimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
